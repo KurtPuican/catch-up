b236bb2c70c683d062ed61b3e8f1873641a74cef
TypeScriptReview <u20201b782@upc.edu.pe>
2023-09-05 09:04:12 -0500

    chore: added news api service client

---
+++ src/app/news/services/news-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  apiKey: string ='d5cd246500eb4b149b447b618c47da9a'
+  baseUrl: string= 'https://newsapi.org/v2';
+  constructor(http: HttpClient) { }
+
+  getSources(){
+    return this.http.get(`${this.baseUrl}/sources?apiKey=${this.apiKey}`);
+  }
+  getArticlesBySourceId(sourceId:string){
+    return this.http.get(`${this.baseUrl}/top-headlines?sources=${sourceId}&aoiKey=${this.apiKey}`);
+  }
+  initiArticles(){
+    return this.getArticlesBySourceId('bbc-news');
+  }
+}
--- src/app/shared/services/logo-api.service.ts
+++ src/app/shared/services/logo-api.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LogoApiService {
-  baseUrl:string ='https//logo.clearbit.com';
+  baseUrl: string ='https//logo.clearbit.com';
 
   constructor() { }
 
